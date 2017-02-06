88b9888491359b9984af24394f01eee37156649e
liwen <1819270694@qq.com>
2017-02-06 18:40:59 +0800

    init project

---
+++ js/es6_export.js
@@ -0,0 +1,8 @@
+// a.js
+export default function() {}
+export function a () {}
+
+var b = 'xxx';
+export {b}; // 这是ES6的写法，实际上就是{b:b}
+setTimeout(() => b = 'ooo', 1000);
+export var c = 100;
---
+++ js/es6_import.js
@@ -0,0 +1,4 @@
+import {a,b,c} from './es6_export';
+console.log(a);
+console.log(b);
+console.log(c);
